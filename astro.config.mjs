e36351a316a03f2c83b2d62b3397152a3b712be3
Calci <Calcifer1331@hotmail.com>
2025-03-10 16:51:22 -0500

    responsive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,40 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [compressor({
+    gzip: true,
+    brotli: true,
+    fileExtensions: [
+      ".css",
+      ".js",
+      ".html",
+      ".xml",
+      ".cjs",
+      ".mjs",
+      ".svg",
+      ".txt",
+      ".json", // Agregados
+      ".webmanifest",
+      ".csv",
+      ".woff",
+      ".woff2",
+      ".ttf",
+      ".geojson",
+    ],
+  })],
+
+  experimental: {
+    svg: {
+      mode: "sprite",
+    },
+  },
 });
